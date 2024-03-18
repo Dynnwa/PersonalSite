0d2b2fe96727053752fb090475c054e5595eca18
Dynnwa <ddaniel.wei@gmail.com>
2024-03-17 20:24:09 -0700

    routing fix

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Switch } from 'react-router-dom';
 import About from './pages/About';
 import Experience from './pages/Experience';
 import Portfolio from './pages/Portfolio';
@@ -8,12 +8,20 @@ import Landing from './Landing';
 const App = () => {
   return (
       <Router>
-        <Routes>
-          <Route exact path="/" element={<Landing />} />
-          <Route exact path="/about" element={<About />} />
-          <Route exact path="/experience" element={<Experience />} />
-          <Route exact path="/portfolio" element={<Portfolio />} />
-        </Routes>
+        <Switch>
+          <Route exact path="/">
+            <Landing />
+          </Route>
+          <Route exact path="/about">
+            <About />
+          </Route>
+          <Route exact path="/experience">
+            <Experience />
+          </Route>
+          <Route exact path="/portfolio">
+            <Portfolio />
+          </Route>
+        </Switch>
       </Router>
   );
 };
